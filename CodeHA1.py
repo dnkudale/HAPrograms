ff50ab95e13763b17384f5587817ddc71b01d31b
Dnyanesh Kudale <dnkudale97@gmail.com>
2020-09-09 21:44:44 +0530

    Some Code Changes

--- CodeHA1.py
+++ CodeHA1.py
@@ -3,6 +3,27 @@ Author : dnkudaled
 Problem : 
 Description : 
 '''
-T = int(input("Enter No. of Testcases : "))
+import sys as sys
+#T = int(input("Enter No. of Testcases : "))
+
+# arr = [3,5,6,1,4]
+# print(arr[-1])
+# float
+
+# double
+
+a = 7
+print(a.__str__())
+print(type(a))
+
+# a = {1:6,2:4}
+
+# print(a.get(4))
+
+# def f():
+#     f()
+#     return 42
 
 
+# f()
+print(sys.path)
